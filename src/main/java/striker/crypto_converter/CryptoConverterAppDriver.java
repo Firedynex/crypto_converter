d7d6d6f1fd38406d00ba2bac6a166c9adaf86328
Firedynex <turjoy10@gmail.com>
2023-05-15 21:57:54 -0400

    Updated docs

--- src/main/java/striker/crypto_converter/CryptoConverterAppDriver.java
+++ src/main/java/striker/crypto_converter/CryptoConverterAppDriver.java
@@ -1,20 +1,9 @@
 package striker.crypto_converter;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 /**
- * JavaFX App
+ * Driver class to run the app.
  */
 public class CryptoConverterAppDriver {
-
-    private static Scene scene;
-
     public static void main(String[] args) {
         Converter converter = new Converter();
     }
